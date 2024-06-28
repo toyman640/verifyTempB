160cc2381bcb5c56016ae3d1848198ee76712142
toyman640 <falakoadebayor@gmail.com>
2024-06-28 14:33:43 +0100

    Modify devise fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname lastname])
+  end
 end
